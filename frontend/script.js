e9d88780ec2aec55457457b9da00c2a2c3057628
shamsharoon <shamsuddin.haroon@ontariotechu.net>
2024-05-09 21:31:16 -0400

    Allow a more friendly UI experience

--- frontend/script.js
+++ frontend/script.js
@@ -96,3 +96,33 @@ function clearResultBox(){
 function isValidBinary(binary) {
     return /^[01]+$/.test(binary);
 }
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open'); // Toggle the menu visibility
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open'); // Close the menu when an option is selected
+
+            options.forEach(option => {
+                option.classList.remove('active');
+            });
+            option.classList.add('active');
+        });
+    });
+});
